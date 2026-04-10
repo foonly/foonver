dde878fe609ae378c15b4932daaca6abbe350fe7
Niklas Schönberg <niklas@uplink.fi>
2026-04-10 16:13:30 +0300

    fix: Change some error messages to print to stderr instead of stdout

--- internal/config/config.go
+++ internal/config/config.go
@@ -101,9 +101,9 @@ func Init() {
 	if err != nil {
 		var configNotFound viper.ConfigFileNotFoundError
 		if errors.As(err, &configNotFound) {
-			fmt.Printf("No config file found, using defaults\n")
+			fmt.Fprintf(os.Stderr, "No config file found, using defaults\n")
 		} else {
-			fmt.Printf("Error reading config file: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Error reading config file: %v\n", err)
 			os.Exit(1)
 		}
 	}
@@ -124,7 +124,7 @@ func Init() {
 		),
 	))
 	if err != nil {
-		fmt.Printf("Error unmarshalling config: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error unmarshalling config: %v\n", err)
 	}
 
 	processFlags()
